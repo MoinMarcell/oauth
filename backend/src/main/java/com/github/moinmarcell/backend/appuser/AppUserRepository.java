fbb00ce3b251063126d0c209143716800714feaf
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-11-10 11:47:58 +0100

    Roles, db, AppUser

--- backend/src/main/java/com/github/moinmarcell/backend/appuser/AppUserRepository.java
+++ backend/src/main/java/com/github/moinmarcell/backend/appuser/AppUserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface AppUserRepository extends MongoRepository<AppUser, String> {
-	boolean existsAppUserByGithubId(int githubId);
 
 	Optional<AppUser> getAppUserByGithubId(int githubId);
 }
--- backend/src/main/java/com/github/moinmarcell/backend/security/AuthController.java
+++ backend/src/main/java/com/github/moinmarcell/backend/security/AuthController.java
@@ -1,25 +1,33 @@
 package com.github.moinmarcell.backend.security;
 
 import com.github.moinmarcell.backend.appuser.AppUser;
+import com.github.moinmarcell.backend.appuser.AppUserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RequestMapping("/api/v1/auth")
 @RestController
 @AllArgsConstructor
 public class AuthController {
 
-	private final AuthService authService;
+	private final AppUserRepository appUserRepository;
 
 	@GetMapping("/me")
-	public AppUser getMe(
-			@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient
-	) {
-		return authService.getLoggedInUser(authorizedClient);
-	}
+	public AppUser getMe() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
+		if (auth instanceof OAuth2AuthenticationToken token) {
+			return appUserRepository.getAppUserByGithubId(Integer.parseInt(token.getPrincipal().getName()))
+					.orElseThrow(() -> new NoSuchElementException("User not found!"));
+		}
+
+		return null;
+	}
 }
